ae7ca633f2b8284258398e779c4c873637e8cf48
liquidtrends <ryan@liquidtrends.com>
2026-01-07 12:36:52 -0700

    Edit tagline

--- components/Tagline.tsx
+++ components/Tagline.tsx
@@ -1,7 +1,7 @@
 export default function Tagline() {
   return (
-    <section className="bg-[#5C0003] text-white text-center py-16 px-6">
-      <p className="text-2xl font-semibold">
+    <section className="bg-[#5C0003] text-white text-center py-16 px-6 ">
+      <p className="text-3xl font-semibold max-w-6xl mx-auto">
         At Homefire Consulting, we believe that meaningful change begins with people, relationships, and culture. 
       </p>
     </section>
