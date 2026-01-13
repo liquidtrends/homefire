b4d3fbfb2add9f4eab06bbc8ecccf0d3590c9c51
liquidtrends <ryan@liquidtrends.com>
2026-01-13 15:15:36 -0700

    edit config

--- next.config.ts
+++ next.config.ts
@@ -1,9 +1,10 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {
+import type { NextConfig } from 'next'
+
+const nextConfig: NextConfig = {
   output: 'export',
   images: {
-    unoptimized: true, // REQUIRED for Cloudflare Pages
+    unoptimized: true,
   },
-};
+}
 
-module.exports = nextConfig;
+export default nextConfig
